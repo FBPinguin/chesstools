b78f1da995b7bf41d8f0a7ecd3ab5eaa8d982602
BuildTools <unconfigured@null.spigotmc.org>
2024-11-18 10:06:48 +0100

    Updates

---
+++ _app/immutable/chunks/legacy.BGAqXxNF.js
@@ -0,0 +1,1 @@
+import{ac as a}from"./runtime.BIG9PkTb.js";a();
---
+++ _app/immutable/nodes/2.FRWPxnqj.js
@@ -0,0 +1,1 @@
+import{t as p,a as o}from"../chunks/disclose-version.CHg3_Yus.js";import"../chunks/legacy.BGAqXxNF.js";import{n as r}from"../chunks/runtime.BIG9PkTb.js";import"../chunks/tooltip.CLHeSTNY.js";function s(t){r();var e=p("Its pretty empty here huh?");o(t,e)}export{s as component};
